e3461603a38f9a3a417c5f6a3ef75739a62be20b
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-02-09 18:41:46 +0000

    aapl history for the past week

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 import yfinance as yf
+import pandas as pd
+
+aapl = yf.Ticker("aapl")
+aapl_historical = aapl.history(start="2023-02-01", end="2023-02-08", interval="1d", actions=False)
+print(aapl_historical)
