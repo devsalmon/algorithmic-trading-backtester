a1a8275d105adec25b177c0de3cbfc7a45e026f4
devsalmon <54852257+devsalmon@users.noreply.github.com>
2023-02-13 20:28:58 +0000

    Update PortfolioConstructor.py

--- Classes/PortfolioConstructor.py
+++ Classes/PortfolioConstructor.py
@@ -14,78 +14,47 @@ import datetime as dt
 class PortfolioConstructor():
     def __init__(self, trades):
         super().__init__()
-        self.portfolioValue = 10_000
-        # First get all different tickers inside our trades and put them in a list,
-        # as we can pass this list into yf.download and will pull all data for each
-        # stock in one go. For now we will just use AAPL and come back to this.
-        self.trades = trades
-        ##self.data = yf.download('AAPL','2013-01-01','2016-08-01',progress=False)
-        # self.df = pd.DataFrame(index=['date'], columns = ['value'])
-        # Then we need a for loop for every date..
-
-        date = pd.bdate_range("20130102", periods=12)
-        df = pd.DataFrame(index=date, columns = ['value']).fillna(0)
-        
-        # Created column for apple
-        df['AAPL'] = 0
-        #print(dt.date(2023,1,4))
-        df['AAPL'].loc[dt.date(2013,1,4):dt.date(2013,1,9)] = 10
-        apple_vector = yf.download("AAPL",dt.date(2013,1,2),dt.date(2013,1,17))
-        df['AAPL'] = df['AAPL'] * apple_vector['Adj Close']
-
-
-        df['value'] += df['AAPL']
-        print(df)
-        
-        
-        # for index, row in df.iterrows():
-        #     date = str(index.date())
-
-        #     stock_val = self.data.loc[date]["Open"]
-        #     df.at[date, 'value'] = stock_val
-
-        # print(df)
-
-        # new_df = pd.DataFrame(index=[len(self.df)], columns = ['value'])
-        # new_df["value"] = 21
-        # self.df = pd.concat([self.df, new_df])
+        #self.portfolioValue = 10_000
+        # Get all different tickers traded and add to ticker set.
+        self.tickers = set()
+        for trade in trades:
+            utid, ticker, qty, buy_date, sell_date = trade
+            if ticker not in self.tickers:
+                self.tickers.add(ticker)
+        # Set the dataframe columns to the tickers and a value column.
+        columns = list(self.tickers)
+        columns.append('value')
+        # Choose date range for backtesting with periods being how many days ahead.
+        date = pd.bdate_range("20130102", periods=10)
+        # Create your dataframe with date being the index and fill in values as 0.
+        df = pd.DataFrame(index=date, columns = columns).fillna(0)
+        # Get data for each traded ticker from yfinance between date ranges.
+        data = yf.download(list(self.tickers),dt.date(2013,1,2),dt.date(2013,1,17))
+        # Set each value in the dataframe with the quantity of stock bought.
+        for trade in trades:
+            utid, ticker, qty, buy_date, sell_date = trade
+            df[ticker].loc[buy_date:sell_date] = qty
+        # Use price of each stock from the yfinance data and use vectorisation to multiply
+        # this by the quantity of the stock we have for each time series.
+        for ticker in self.tickers:
+            df[ticker] = df[ticker] * data['Adj Close'][ticker]
+        # Add up each column to get total value column for each time series.
+        for ticker in self.tickers:
+            df['value'] += df[ticker]
+        # Add dataframe as an object attribute.
+        self.df = df
     
-    def print_data_frame(self):
-        pass
-        # print(self.df)
-        #print(self.data)
-    
-
-    # def calc(self, trades):
-    #     for trade in trades:
-    #         utid, ticker, qty, buy_date, sell_date = trade
-
-    #         buy_price, sell_price = self.get_buy_sell_prices(ticker, buy_date, sell_date)
-
-    #         self.calc_pnl(qty, buy_price, sell_price)
-
-    # def get_buy_sell_prices(self, ticker, buy_date, sell_date):
-    #     df = yf.download(ticker, buy_date, sell_date,interval="1d")
-    #     # Assuming buying at open, and selling at close
-    #     buy_price, sell_price = df["Open"][0], df["Close"][-1]
-    #     return buy_price, sell_price
-
-    # def calc_pnl(self, qty, buy_price, sell_price):
-    #     self.portfolioValue += qty*(sell_price - buy_price)
-
-    # def print_trades(self):
-    #     print(self.trades)
+    def print_dataframe(self):
+        print(self.df)
 
 
 # UTID:  Ticker:  Quantity:  Buy Date:  Sell Date:
 trades = [
-    [1, "AAPL", 10, "2020-01-02", "2020-01-03"],
-    [2, "AAPL", 10, "2020-01-05", "2020-01-07"],
-    [3, "AAPL", 10, "2020-01-06", "2020-01-07"],
-    [4, "AAPL", 10, "2020-01-08", "2020-01-09"],
+    [1, "AAPL", 10, dt.date(2013,1,4), dt.date(2013,1,8)],
+    [3, "MSFT", 10, dt.date(2013,1,3), dt.date(2013,1,5)],
+    [4, "AAPL", 10, dt.date(2013,1,8), dt.date(2013,1,14)],
+    [2, "GBPUSD=X", 10, dt.date(2013,1,2), dt.date(2013,1,4)]
     ]
 
 pc = PortfolioConstructor(trades)
-pc.print_data_frame()
-# pc.calc(trades)
-# print(pc.portfolioValue)
+pc.print_dataframe()
