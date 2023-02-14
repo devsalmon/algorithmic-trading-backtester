229e1af6bd4cb5197181ceac441c1ae4af5e818f
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-02-14 13:46:26 +0000

    Subtracted value of trade from cash at inception

--- Classes/PortfolioConstructor.py
+++ Classes/PortfolioConstructor.py
@@ -11,14 +11,17 @@ import yfinance as yf
 import pandas as pd
 import datetime as dt
 
+
+# TODO - implement 'cash' column and leverage
 class PortfolioConstructor():
     def __init__(self, trades):
         super().__init__()
+        self.cashValue = 10_000
         #self.portfolioValue = 10_000
         # Get all different tickers traded and add to ticker set.
         self.tickers = set()
         for trade in trades:
-            utid, ticker, qty, buy_date, sell_date = trade
+            utid, ticker, qty, leverage, buy_date, sell_date = trade
             if ticker not in self.tickers:
                 self.tickers.add(ticker)
         # Set the dataframe columns to the tickers and a value column.
@@ -33,17 +36,32 @@ class PortfolioConstructor():
         data = yf.download(list(self.tickers),dt.date(2013,1,2),dt.date(2013,1,17), progress=False)
         # Set each value in the dataframe with the quantity of stock bought.
         for trade in trades:
-            utid, ticker, qty, buy_date, sell_date = trade
-            df[ticker].loc[buy_date:sell_date] = qty
+            utid, ticker, qty, leverage, buy_date, sell_date = trade
+            df[ticker].loc[buy_date:sell_date] = qty*leverage
+            
+            # Subtract the value of the trade on each buy, for the whole date range
+            df['cash'].loc[buy_date:sell_date] -= qty*data["Open"][ticker][str(buy_date)]
+
+
         # Use price of each stock from the yfinance data and use vectorisation to multiply
         # this by the quantity of the stock we have for each time series.
         for ticker in self.tickers:
             df[ticker] *= data['Adj Close'][ticker]
+            
+        # ----------------------------------------------------------------
+
+        # TODO - for the sell date for each trade, add the current value of the trade
+        for trade in trades:
+            utid, ticker, qty, leverage, buy_date, sell_date = trade
+            # df['cash'].loc[sell_date] += qty*data["Adj Close"][ticker][sell_date]   
+
+        # ----------------------------------------------------------------z
+
         # Add up each column to get total value column for each time series.
         for ticker in self.tickers:
             df['value'] += df[ticker]
-            
-        df['value'] += df['cash']
+
+        # df['value'] += df['cash']
         # Add dataframe as an object attribute.
         self.df = df
     
@@ -51,12 +69,12 @@ class PortfolioConstructor():
         print(self.df)
 
 
-# UTID:  Ticker:  Quantity:  Buy Date:  Sell Date:
+# UTID:  Ticker:  Quantity:  Leverage: Buy Date:  Sell Date:
 trades = [
-    [1, "AAPL", 10, dt.date(2013,1,4), dt.date(2013,1,8)],
-    [3, "MSFT", 10, dt.date(2013,1,3), dt.date(2013,1,5)],
-    [4, "AAPL", 10, dt.date(2013,1,8), dt.date(2013,1,14)],
-    [2, "GBPUSD=X", 10, dt.date(2013,1,2), dt.date(2013,1,4)]
+    [1, "AAPL", 10, 1, dt.date(2013,1,4), dt.date(2013,1,8)],
+    [3, "MSFT", 10, 1, dt.date(2013,1,3), dt.date(2013,1,5)],
+    [4, "AAPL", 10, 1, dt.date(2013,1,8), dt.date(2013,1,14)],
+    [2, "GBPUSD=X", 10, 1, dt.date(2013,1,2), dt.date(2013,1,4)]
     ]
 
 pc = PortfolioConstructor(trades)
